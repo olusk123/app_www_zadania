a8bb8383462d8cab4324363947154374b90b4bf5
olusk123 <166287@student.uwm.edu.pl>
2024-10-23 22:30:53 +0200

    Summary

--- lab2/FirstApp/models.py
+++ lab2/FirstApp/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products')
     available = models.BooleanField(default=True)
-    stock = models.PositiveIntegerField(default=0)  # Nowe pole
+    stock = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
